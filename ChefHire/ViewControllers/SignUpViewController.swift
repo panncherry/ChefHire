87a437ea4ffca8036f516ad130c3b97226e89c9b
panncherry <maung.eainmatmay@gmail.com>
2019-04-09 22:46:49 -0400

    Create outlets for SignUpViewControllers

--- ChefHire/ViewControllers/SignUpViewController.swift
+++ ChefHire/ViewControllers/SignUpViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import Parse
 
 class SignUpViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNameTxtField: UITextField!
+    @IBOutlet weak var lastNameTxtField: UITextField!
+    @IBOutlet weak var userNameTxtField: UITextField!
+    @IBOutlet weak var emailAddressTxtField: UITextField!
+    @IBOutlet weak var passwordTxtField: UITextField!
+    @IBOutlet weak var repeatPasswordTxtField: UITextField!
+    @IBOutlet weak var createAccountBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
+    
+    
+    /*:
+     # Create Account
+     * Create a new account when clicked on createAccountBtn
+     */
+    @IBAction func onClick_CreateAccount(_ sender: Any) {
+        let user = PFUser()
+       
+    }
+    
+    
+    /*:
+     # Cancel Creating Account
+     * Dismiss SignUpViewController
+     * Display LogInViewController
+     * Clear all text fields
+     */
+    @IBAction func onClick_Cancel(_ sender: Any) {
+    }
 }
